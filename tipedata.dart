a6acc489826069cfc74b66dd6e3f1771ec73c233
roypahri-coder <roypahri@gmail.com>
2026-08-31 20:32:08 +0700

    (update N)

--- tipedata.dart
+++ tipedata.dart
@@ -1,10 +1,67 @@
+import 'dart:ffi';
+
 void main (){
-  String nama = 'Prabowo';
-  int umur = 100;
-  bool belummenikah = false;
+//  String nama = 'Prabowo';
+ // int umur = 100;
+ // bool belummenikah = false;
 
-  var nama1 = 'Deni';
-  var umur1 =  100;
+ // var nama1 = 'Deni';
+  //var umur1 =  100;
 
-  print ('halo nama saya $nama. umur saya $umur.sudah menikah $belummenikah ');
+  //print ('halo nama saya $nama. umur saya $umur.sudah menikah $belummenikah ');
+
+  // int angka1= 200;
+  // double angka2= 20.5;
+  // num hasil = 0;
+
+  // hasil = angka1 + angka2;
+  // print(hasil);
+
+
+  // list (array)
+  // List <dynamic> Namamhs= ['Prabowo','Jokowi',100, true];
+  // List <String> nama = ['Jokowowo','Gibran Pancaroba'];
+  // List <num> angka = [12, 12.5, 13.0,-1, -12.5];
+
+  // print (Namamhs[0]);
+
+  // List namamhs = [];
+  // namamhs.add('Jokowi');
+  // namamhs.add('Jokwo');
+  // namamhs.add('Joko');
+  // namamhs.add('gibran');
+  // // namamhs.removeAt(3);
+  // print(namamhs.indexOf('gibran'));
+
+
+//// set
+ 
+/// map
+   //Map datamhs = 
+   // {'Nama':'Prabowo','umur':30, 'kelas':'Presiden'};
+   //{'Nama':'Gibran','umur':20, 'kelas':'W.P'};
+   //{'Nama':'Joko','umur':10, 'kelas':'AFK'};
+   
+   //void main() {
+  // Gabungkan semua Map ke dalam sebuah List []
+  // List<Map> datamhs = [
+  //   {'Nama': 'Prabowo', 'umur': 30, 'kelas': 'Presiden'},
+  //   {'Nama': 'Gibran', 'umur': 20, 'kelas': 'W.P'},
+  //   {'Nama': 'Joko', 'umur': 10, 'kelas': 'AFK'},
+  // ];
+
+  // // Langsung cetak variabel list-nya
+  // print('halo ${datamhs[1][]},');
+
+ // final String nama;
+ // nama= 'Prabowo';
+
+ // const int angka = 100; // penggunaan bersifat absolut
+ 
+
+ //dart => Null safety
+ //string? nama;
+ //int? angka;
+ //print(angka);
+ 
 }
